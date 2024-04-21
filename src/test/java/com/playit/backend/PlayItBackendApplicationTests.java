09ca0903fe5ddfe24392a4b24a4a16e5cf4e94e1
Martin Bonnefoy <martin.bonnefoy@gmail.com>
2024-04-21 18:31:58 +0200

    Test d'utilisation des websockets: - avec STOMP - avec un protocole custom

--- src/test/java/com/playit/backend/PlayItBackendApplicationTests.java
+++ src/test/java/com/playit/backend/PlayItBackendApplicationTests.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -21,10 +20,23 @@ class PlayItBackendApplicationTests {
 
 	@Test
 	void getEntryPoint() throws Exception {
-		this.mvc.perform(MockMvcRequestBuilders.get("/")
-											   .accept(MediaType.APPLICATION_JSON))
+		this.mvc.perform(MockMvcRequestBuilders.get("/"))
 				.andExpect(status().isOk())
 				.andExpect(content().string(equalTo("Incroyable")));
+	}
+
+	@Test
+	void getHelloWorld() throws Exception {
+		this.mvc.perform(MockMvcRequestBuilders.get("/hello"))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Hello World!")));
+	}
+
+	@Test
+	void getHelloWorldWithParameter() throws Exception {
+		this.mvc.perform(MockMvcRequestBuilders.get("/hello?name=Martin"))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Hello Martin!")));
 	}
 
 }
