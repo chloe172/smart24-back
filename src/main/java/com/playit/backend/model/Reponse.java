2d1ef1f95c8eb74ebe1cfed76646ca55a18c9293
Martin Bonnefoy <martin.bonnefoy@gmail.com>
2024-04-30 11:38:21 +0200

    Fix error

--- src/main/java/com/playit/backend/model/Reponse.java
+++ src/main/java/com/playit/backend/model/Reponse.java
@@ -1,154 +1,132 @@
 package com.playit.backend.model;
 
-import jakarta.persistence.ManyToOne;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
-
-import java.time.LocalDateTime;
-import java.time.Duration;
 import jakarta.persistence.UniqueConstraint;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(
-    uniqueConstraints = {
-        @UniqueConstraint(name="reponseunique", columnNames={"equipe", "activiteEnCours"})
-        }
-        )
+@Table(uniqueConstraints = { @UniqueConstraint(name = "reponseunique", columnNames = { "equipe", "activiteEnCours" }) })
 public class Reponse {
-    @Id
-	@GeneratedValue(strategy = GenerationType.TABLE)
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-    @Temporal(TemporalType.TIMESTAMP)
-    private LocalDateTime dateSoumission = LocalDateTime.now();
-    private int scoreEquipe;
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalDateTime dateSoumission = LocalDateTime.now();
+	private int scoreEquipe;
 
-    @ManyToOne
-    private Proposition proposition;
+	@ManyToOne
+	private Proposition proposition;
 
-    @ManyToOne
-    private Equipe equipe;
+	@ManyToOne
+	private Equipe equipe;
 
-    @ManyToOne
-    private ActiviteEnCours activiteEnCours;
-    
-    public Reponse(){
-    }
-    
-    public void calculerScoreEquipe() {
-        Question question = ((Question) activiteEnCours.getActivite());
-        Proposition bonneProposition = new Proposition();
-        if(question instanceof QuestionQCM){
-            bonneProposition = ((QuestionQCM) question).getBonneProposition();
-        } else if(question instanceof QuestionVraiFaux) {
-            bonneProposition = ((QuestionVraiFaux) question).getBonneProposition();
-        }
+	@ManyToOne
+	private ActiviteEnCours activiteEnCours;
 
-        if(bonneProposition.equals(proposition)) {
-            int scoreMax = question.getScore();
-            scoreEquipe = scoreMax / 2;
-                        
-            LocalDateTime debutQuestion = activiteEnCours.getDate();
-            Duration dureeQuestion = question.getTemps();
-            LocalDateTime finQuestion = debutQuestion.plus(dureeQuestion);
-        
-            // Normalisation du temps restant
-            Duration tempsRestant = Duration.between(finQuestion, dateSoumission);
-            long coefficient = tempsRestant.dividedBy(dureeQuestion);
+	public Reponse() {
+	}
 
-            scoreEquipe += coefficient * (scoreMax / 2);
-        } else {
-            scoreEquipe = 0;
-        }
+	public void calculerScoreEquipe() {
+		Question question = ((Question) this.activiteEnCours.getActivite());
+		Proposition bonneProposition = new Proposition();
+		if (question instanceof QuestionQCM) {
+			bonneProposition = ((QuestionQCM) question).getBonneProposition();
+		} else if (question instanceof QuestionVraiFaux) {
+			bonneProposition = ((QuestionVraiFaux) question).getBonneProposition();
+		}
 
-        equipe.ajouterScore(scoreEquipe);
-    }
-    public LocalDateTime getDateSoumission() {
-        return dateSoumission;
-    }
+		if (bonneProposition.equals(this.proposition)) {
+			int scoreMax = question.getScore();
+			this.scoreEquipe = scoreMax / 2;
 
-    public void setDateSoumission(LocalDateTime dateSoumission) {
-        this.dateSoumission = dateSoumission;
-    }
+			LocalDateTime debutQuestion = this.activiteEnCours.getDate();
+			Duration dureeQuestion = question.getTemps();
+			LocalDateTime finQuestion = debutQuestion.plus(dureeQuestion);
 
-    public Proposition getProposition() {
-        return proposition;
-    }
+			// Normalisation du temps restant
+			Duration tempsRestant = Duration.between(finQuestion, this.dateSoumission);
+			long coefficient = tempsRestant.dividedBy(dureeQuestion);
 
-    public void setProposition(Proposition proposition) {
-        this.proposition = proposition;
-    }
+			this.scoreEquipe += coefficient * (scoreMax / 2);
+		} else {
+			this.scoreEquipe = 0;
+		}
 
-    public Equipe getEquipe() {
-        return equipe;
-    }
+		this.equipe.ajouterScore(this.scoreEquipe);
+	}
 
-    public void setEquipe(Equipe equipe) {
-        this.equipe = equipe;
-    }
+	public LocalDateTime getDateSoumission() {
+		return this.dateSoumission;
+	}
 
-    public int getScoreEquipe() {
-        return scoreEquipe;
-    }
+	public void setDateSoumission(LocalDateTime dateSoumission) {
+		this.dateSoumission = dateSoumission;
+	}
 
-    public void setScoreEquipe(int scoreEquipe) {
-        this.scoreEquipe = scoreEquipe;
-    }
+	public Proposition getProposition() {
+		return this.proposition;
+	}
 
-    public Long getId() {
-        return id;
-    }
-    
-    public ActiviteEnCours getActiviteEnCours() {
-        return activiteEnCours;
-    }
+	public void setProposition(Proposition proposition) {
+		this.proposition = proposition;
+	}
 
-    public void setActiviteEnCours(ActiviteEnCours activiteEnCours) {
-        this.activiteEnCours = activiteEnCours;
-    }
+	public Equipe getEquipe() {
+		return this.equipe;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Reponse other = (Reponse) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        if (dateSoumission == null) {
-            if (other.dateSoumission != null)
-                return false;
-        } else if (!dateSoumission.equals(other.dateSoumission))
-            return false;
-        if (scoreEquipe != other.scoreEquipe)
-            return false;
-        if (proposition == null) {
-            if (other.proposition != null)
-                return false;
-        } else if (!proposition.equals(other.proposition))
-            return false;
-        if (equipe == null) {
-            if (other.equipe != null)
-                return false;
-        } else if (!equipe.equals(other.equipe))
-            return false;
-        if (activiteEnCours == null) {
-            if (other.activiteEnCours != null)
-                return false;
-        } else if (!activiteEnCours.equals(other.activiteEnCours))
-            return false;
-        return true;
-    }
-    
-    
+	public void setEquipe(Equipe equipe) {
+		this.equipe = equipe;
+	}
+
+	public int getScoreEquipe() {
+		return this.scoreEquipe;
+	}
+
+	public void setScoreEquipe(int scoreEquipe) {
+		this.scoreEquipe = scoreEquipe;
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public ActiviteEnCours getActiviteEnCours() {
+		return this.activiteEnCours;
+	}
+
+	public void setActiviteEnCours(ActiviteEnCours activiteEnCours) {
+		this.activiteEnCours = activiteEnCours;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if ((obj == null) || (this.getClass() != obj.getClass())) {
+			return false;
+		}
+		Reponse other = (Reponse) obj;
+		if (!Objects.equals(this.id, other.id) || !Objects.equals(this.dateSoumission, other.dateSoumission)
+			|| (this.scoreEquipe != other.scoreEquipe) || !Objects.equals(this.proposition, other.proposition)) {
+			return false;
+		}
+		if (!Objects.equals(this.equipe, other.equipe)
+			|| !Objects.equals(this.activiteEnCours, other.activiteEnCours)) {
+			return false;
+		}
+		return true;
+	}
+
 }
